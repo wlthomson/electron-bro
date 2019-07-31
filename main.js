759d2c5bc3f5d868ca95d6c86b8716e0d8080238
Will Thomson <will.tho@outlook.com>
2019-07-31 20:53:10 +1200

    Add prettier, eslint

--- main.js
+++ main.js
@@ -1,42 +1,44 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
-const fs = require('fs')
+const { app, BrowserWindow, ipcMain } = require('electron');
+const fs = require('fs');
 
-let win
+let win;
 
 const createWindow = () => {
   win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
-    }
-  })
+      nodeIntegration: true,
+    },
+  });
 
-  win.loadFile('index.html')
+  win.loadFile('index.html');
 
   win.on('closed', () => {
-    win = null
-  })
-}
+    win = null;
+  });
+};
 
-const saveReport = async (report) => {
-  fs.writeFile('./report.json', JSON.stringify(report), (err) => { if (err) throw err; } )
-}
+const saveReport = report => {
+  fs.writeFile('./report.json', JSON.stringify(report), err => {
+    if (err) throw err;
+  });
+};
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
-ipcMain.on('call-saveReport', (_, report) => { 
-  saveReport(report)
+ipcMain.on('call-saveReport', (_, report) => {
+  saveReport(report);
 });
